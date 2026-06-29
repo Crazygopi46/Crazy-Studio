740c88db93535c49c621d2df8daa04c852bf29a1
Crazy Studio <gopiofficial2005@gmail.com>
2026-06-29 12:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+// Crazy Studio Website
 
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// Button Animation
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("mouseenter",()=>{
+    btn.style.transform="scale(1.1)";
+});
+
+btn.addEventListener("mouseleave",()=>{
+    btn.style.transform="scale(1)";
+});
